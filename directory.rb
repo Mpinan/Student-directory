7f829a246d80cadc51a60ea05480ea4efd24d4a3
Mario90 <mgpinan1990@gmail.com>
2019-07-16 22:09:33 +0100

    Step 5

--- directory.rb
+++ directory.rb
@@ -1,16 +1,30 @@
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarte",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Jocker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+def print_header
 puts "The students of Villains Academy"
-puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarte"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Jocker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have "
-print 11
-puts " great students"
+puts "-------------"
+end
+
+def print(names)
+  names.each do |name| puts name
+  end
+end
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
